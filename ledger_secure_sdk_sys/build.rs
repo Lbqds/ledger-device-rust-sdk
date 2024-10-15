9146dc8b85118a48eb8692d08ee6232041179c96
lbqds <lbqds@outlook.com>
2024-10-15 11:00:50 +0800

    wip

--- ledger_secure_sdk_sys/build.rs
+++ ledger_secure_sdk_sys/build.rs
@@ -697,32 +697,30 @@ fn finalize_nanosplus_configuration(command: &mut cc::Build, bolos_sdk: &Path) {
 }
 
 fn configure_lib_bagl(command: &mut cc::Build, bolos_sdk: &Path) {
-    if env::var_os("CARGO_FEATURE_LIB_BAGL").is_some() {
-        println!(">>>>>>>>>>>>>>>>>>> cargo feature lib bagl");
-        command
-            .define("HAVE_BAGL", None)
-            // Just include all the fonts for now; we can shrink the X and S+ images later.
-            .define("HAVE_BAGL_FONT_LUCIDA_CONSOLE_8PX", None)
-            .define("HAVE_BAGL_FONT_OPEN_SANS_LIGHT_16_22PX", None)
-            .define("HAVE_BAGL_FONT_OPEN_SANS_REGULAR_8_11PX", None)
-            .define("HAVE_BAGL_FONT_OPEN_SANS_REGULAR_10_13PX", None)
-            .define("HAVE_BAGL_FONT_OPEN_SANS_REGULAR_11_14PX", None)
-            .define("HAVE_BAGL_FONT_OPEN_SANS_REGULAR_13_18PX", None)
-            .define("HAVE_BAGL_FONT_OPEN_SANS_REGULAR_22_30PX", None)
-            .define("HAVE_BAGL_FONT_OPEN_SANS_SEMIBOLD_8_11PX", None)
-            .define("HAVE_BAGL_FONT_OPEN_SANS_EXTRABOLD_11PX", None)
-            .define("HAVE_BAGL_FONT_OPEN_SANS_LIGHT_16PX", None)
-            .define("HAVE_BAGL_FONT_OPEN_SANS_REGULAR_11PX", None)
-            .define("HAVE_BAGL_FONT_OPEN_SANS_SEMIBOLD_10_13PX", None)
-            .define("HAVE_BAGL_FONT_OPEN_SANS_SEMIBOLD_11_16PX", None)
-            .define("HAVE_BAGL_FONT_OPEN_SANS_SEMIBOLD_13_18PX", None)
-            .define("HAVE_BAGL_FONT_SYMBOLS_0", None)
-            .define("HAVE_BAGL_FONT_SYMBOLS_1", None)
-            .include(bolos_sdk.join("lib_bagl/include/"))
-            .file(bolos_sdk.join("lib_bagl/src/bagl.c"))
-            .file(bolos_sdk.join("lib_bagl/src/bagl_fonts.c"))
-            .file(bolos_sdk.join("lib_bagl/src/bagl_glyphs.c"));
-    }
+    println!(">>>>>>>>>>>>>>>>>>> cargo feature lib bagl");
+    command
+        .define("HAVE_BAGL", None)
+        // Just include all the fonts for now; we can shrink the X and S+ images later.
+        .define("HAVE_BAGL_FONT_LUCIDA_CONSOLE_8PX", None)
+        .define("HAVE_BAGL_FONT_OPEN_SANS_LIGHT_16_22PX", None)
+        .define("HAVE_BAGL_FONT_OPEN_SANS_REGULAR_8_11PX", None)
+        .define("HAVE_BAGL_FONT_OPEN_SANS_REGULAR_10_13PX", None)
+        .define("HAVE_BAGL_FONT_OPEN_SANS_REGULAR_11_14PX", None)
+        .define("HAVE_BAGL_FONT_OPEN_SANS_REGULAR_13_18PX", None)
+        .define("HAVE_BAGL_FONT_OPEN_SANS_REGULAR_22_30PX", None)
+        .define("HAVE_BAGL_FONT_OPEN_SANS_SEMIBOLD_8_11PX", None)
+        .define("HAVE_BAGL_FONT_OPEN_SANS_EXTRABOLD_11PX", None)
+        .define("HAVE_BAGL_FONT_OPEN_SANS_LIGHT_16PX", None)
+        .define("HAVE_BAGL_FONT_OPEN_SANS_REGULAR_11PX", None)
+        .define("HAVE_BAGL_FONT_OPEN_SANS_SEMIBOLD_10_13PX", None)
+        .define("HAVE_BAGL_FONT_OPEN_SANS_SEMIBOLD_11_16PX", None)
+        .define("HAVE_BAGL_FONT_OPEN_SANS_SEMIBOLD_13_18PX", None)
+        .define("HAVE_BAGL_FONT_SYMBOLS_0", None)
+        .define("HAVE_BAGL_FONT_SYMBOLS_1", None)
+        .include(bolos_sdk.join("lib_bagl/include/"))
+        .file(bolos_sdk.join("lib_bagl/src/bagl.c"))
+        .file(bolos_sdk.join("lib_bagl/src/bagl_fonts.c"))
+        .file(bolos_sdk.join("lib_bagl/src/bagl_glyphs.c"));
 }
 
 fn configure_webusb(command: &mut cc::Build, bolos_sdk: &Path) {
